550089529a7100ecd7cc65fa49f71429d3aadc41
Antony <a.yatsenko@aeronavigator.ru>
2020-04-22 13:02:12 +0300

    Add simple hello world example

---
+++ src/main/java/glim/antony/tutorial/hello/world/Producer.java
@@ -0,0 +1,34 @@
+package glim.antony.tutorial.hello.world;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+/*
+ *
+ * @author a.yatsenko
+ * Created at 22.04.2020
+ *
+ * @see {https://www.rabbitmq.com/tutorials/tutorial-one-java.html}
+ */
+public class Producer {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()
+        ) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+}
